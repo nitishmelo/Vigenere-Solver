32a4bf8addbcc80cd6830b41691307696688f3db
nitishmel <90356171+nitishmel@users.noreply.github.com>
2023-06-25 00:47:27 -0400

    Parameterized Test for Big Text Cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,23 @@
 import os
 import pytest
-from test_complete_break import data  
+from test_complete_break import multipletextsdata, bigtextdata
 
 @pytest.hookimpl()
 def pytest_sessionstart():
     for file in os.listdir("./files"):
-        with open(f"./files/{file}") as fp:
+        if file != "bigtextmultiplekeys.txt":
+            with open(f"./files/{file}") as fp:
+                cipherText = fp.readline().strip('\n')
+                plainText = fp.readline().strip('\n')
+                key = fp.readline().strip('\n')
+                keylen = int(fp.readline())
+                testID = f"Cipher with key of length {keylen}"
+                multipletextsdata.append((cipherText, plainText, key, keylen, testID))
+    with open("./files/bigtextmultiplekeys.txt", encoding='utf-8') as fp:
+        plainText = fp.readline().strip('\n')
+        for _ in range(9):
             cipherText = fp.readline().strip('\n')
-            plainText = fp.readline().strip('\n')
             key = fp.readline().strip('\n')
-            keylen = int(fp.readline())
-            data.append((cipherText, plainText, key, keylen))
+            keylen = int(fp.readline().strip('\n'))
+            testID = f"Cipher with key of length {keylen}"
+            bigtextdata.append((cipherText, plainText, key, keylen, testID))
